13ca515152239b4e1fbd0440d42d30b1591e6f76
Eric Ng <ericnjf@gmail.com>
2019-09-16 00:16:05 +0800

    feat/template: update basic game.hx to create the world

--- src/Game.hx
+++ src/Game.hx
@@ -1,7 +1,8 @@
 
 class Game extends hxd.App {
 
-    var bmp: h2d.Bitmap;
+    var character: ecs.Entity;
+    var world: ecs.World;
 
     override function init() {
         hxd.Res.initEmbed();
@@ -11,25 +12,60 @@ class Game extends hxd.App {
 
         var assetsMap = common.Assets.parseAssets("assets.json");
 
-        this.bmp = assetsMap.getAsset("character").getBitmap();
-        this.s2d.addChild(this.bmp);
-
         // add event handler
         hxd.Window.getInstance().addEventTarget(onEvent);
+
+        this.world = new ecs.World();
+
+        // adding render system
+        var renderSystem = new ecs.Render2D.RenderSystem(this.s2d);
+        this.world.addSystem(renderSystem);
+
+        // add character to the world
+        var comp = new ecs.Render2D.RenderComponent(assetsMap.getAsset("character").getBitmap(), 1);
+        var ent = ecs.Entity.newEntity();
+        ent.addComponent(comp);
+        this.world.addEntity(ent);
+        this.character = ent;
+
+        for (i in 0...10) {
+            var s = new ecs.Space.SpaceComponent(i*32, i*32);
+            var c = new ecs.Render2D.RenderComponent(assetsMap.getAsset("wall").getBitmap());
+            var e = ecs.Entity.newEntity();
+            e.addComponent(s);
+            e.addComponent(c);
+            this.world.addEntity(e);
+        }
     }
 
-    // on each frame
     override function update(dt:Float) {
-        bmp.x += dt * 100;
+    }
+
+    override function render(engine: h3d.Engine) {
+        this.s2d.render(engine);
     }
 
     function onEvent(event: hxd.Event) {
-
         switch(event.kind) {
             case EKeyDown:
+                var space = Std.downcast(
+                    this.character.getComponent(ecs.Space.SpaceComponent.TYPE_STRING),
+                    ecs.Space.SpaceComponent
+                );
                 if (event.keyCode == hxd.Key.S) {
-                    bmp.y += 50;
+                    space.y += 32;
+                } else if (event.keyCode == hxd.Key.A) {
+                    space.x -= 32;
+                } else if (event.keyCode == hxd.Key.W) {
+                    space.y -= 32;
+                } else if (event.keyCode == hxd.Key.D) {
+                    space.x += 32;
+                } else if (event.keyCode == hxd.Key.UP) {
+                } else if (event.keyCode == hxd.Key.DOWN) {
+                } else if (event.keyCode == hxd.Key.LEFT) {
+                } else if (event.keyCode == hxd.Key.RIGHT) {
                 }
+
             case _:
         }
     }
--- src/ecs/README.md
+++ src/ecs/README.md
@@ -4,3 +4,7 @@
 The goal of ECS here is to allow us to build the game logic using ECS.
 HUD and other stuffs should not be rendered using ECS (probably), and might be better off using the basic objects.
 
+# Usage
+
+Current goal is to use this repo as a boilerplate rather than a module.
+Might consider making it a module/package in the future if I need.
