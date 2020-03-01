1447c118e6c0cb2e391d3012b5f8f31c336f544a
Eric Ng <ericnjf@gmail.com>
2020-03-02 00:20:47 +0800

    feat/animation: update templates with some animations for testing

--- src/gamescene/GameScene.hx
+++ src/gamescene/GameScene.hx
@@ -1,6 +1,8 @@
 
 package gamescene;
 
+import common.h2d.Animation.Animator;
+
 class GameScene implements common.Scene {
 
     var assets: common.Assets;
@@ -9,15 +11,46 @@ class GameScene implements common.Scene {
     var backgroundLayer: h2d.Layers;
     var foregroundLayer: h2d.Layers;
 
+    var animator: common.h2d.Animation.Animator;
+
     public function new(assets: common.Assets, console: h2d.Console) {
         this.scene = new h2d.Scene();
         this.assets = assets;
         this.init();
+
+        this.backgroundLayer = new h2d.Layers();
+        this.foregroundLayer = new h2d.Layers();
+        this.scene.add(backgroundLayer, 0);
+        this.scene.add(foregroundLayer, 1);
+
+        this.animator = new common.h2d.Animation.Animator();
+
+        var obj1 = new h2d.Bitmap(h2d.Tile.fromColor(0xFF0000, 32, 32));
+        obj1.x = 32; obj1.y = 32;
+        this.backgroundLayer.add(obj1, 0);
+        this.animator.moveTo(obj1, [128, 32], 32, function() {obj1.visible = false;});
+
+        var obj2 = new h2d.Bitmap(h2d.Tile.fromColor(0x00FF00, 32, 32));
+        obj2.x = 32; obj2.y = 32;
+        this.backgroundLayer.add(obj2, 0);
+        this.animator.moveTo(obj2, [32, 128], 32);
+
+        var obj3 = new h2d.Bitmap(h2d.Tile.fromColor(0xFF0000, 32, 32));
+        obj3.x = 64; obj3.y = 64;
+        this.backgroundLayer.add(obj3, 0);
+        this.animator.moveBy(obj3, [96, 0], 32);
+
+        var obj4 = new h2d.Bitmap(h2d.Tile.fromColor(0x00FF00, 32, 32));
+        obj4.x = 64; obj4.y = 64;
+        this.backgroundLayer.add(obj4, 0);
+        this.animator.moveBy(obj4, [0, 96], 32, function() {obj4.y = 64;});
+
     }
 
     function init() {}
 
     public function update(dt: Float) {
+        animator.update(dt);
     }
 
     public function render(engine: h3d.Engine) {
