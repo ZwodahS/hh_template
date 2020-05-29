406f6f149c0327d355d569e0d16b271a55ea9b73
Eric Ng <ericnjf@gmail.com>
2020-05-29 15:15:21 +0800

    feat/pipeline: add convert asejson pipeline script

---
+++ raw/convert_from_asejson.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+if len(sys.argv) == 1:
+    print("""
+This will convert all the frames in the json into a dictionary,
+filename -> { x, y, w, h, r }
+
+This assumes that the filename is properly set when exporting from aseprite
+    """)
+    print("[0] [name] [out]".format(sys.argv[0]))
+    sys.exit(0)
+
+with open(sys.argv[1]) as f:
+    data = json.loads(f.read())
+
+frames = data["frames"]
+processed_frames = {}
+
+for frame in frames:
+    processed_frames[frame["filename"]] = frame["frame"]
+    processed_frames[frame["filename"]]["r"] = 1 if frame["rotated"] else 0
+
+with open(sys.argv[2], "w") as f:
+    print(json.dumps(processed_frames, indent=2), file=f)
+
+
