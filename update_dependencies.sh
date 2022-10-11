04035a50eafec5f14c416835b11cffd5652ed49f
Eric Ng <ericnjf@gmail.com>
2022-10-11 16:23:10 +0800

    update/dependencies

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ZF=$(haxelib libpath zf)
-HEAPS=$(haxelib libpath heaps-dev)
+HEAPS=$(haxelib libpath heaps)
+ECHO=$(haxelib libpath echo)
+COMPILETIME=$(haxelib libpath compiletime)
+HXRANDOM=$(haxelib libpath hxrandom)
 
 function update() {
     pushd $ZF > /dev/null
@@ -11,6 +14,21 @@ function update() {
     pushd $HEAPS > /dev/null
     echo -n "heaps: "
     git rev-parse heaps-stable
+    popd > /dev/null
+
+    pushd $ECHO > /dev/null
+    echo -n "echo: "
+    git rev-parse HEAD
+    popd > /dev/null
+
+    pushd $COMPILETIME > /dev/null
+    echo -n "compiletime: "
+    git rev-parse HEAD
+    popd > /dev/null
+
+    pushd $HXRANDOM > /dev/null
+    echo -n "hxrandom: "
+    git rev-parse HEAD
     popd > /dev/null
 }
 
