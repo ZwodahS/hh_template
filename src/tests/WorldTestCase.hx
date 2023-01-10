f3205b2b6c87ee672ed8906abef7e378eb7cc794
Eric Ng <ericnjf@gmail.com>
2023-01-10 17:08:41 +0800

    fix/tests: fix tests not working on js

--- src/tests/WorldTestCase.hx
+++ src/tests/WorldTestCase.hx
@@ -6,14 +6,19 @@ class WorldTestCase extends zf.engine2.tests.WorldTestCase {
 	public var profile(default, null): userdata.Profile;
 
 	public function get_world(): World {
-		return cast super.__world__;
+		return cast this.__world__;
 	}
 
 	public function new(testId: String, name: String) {
+		super(testId, name);
+	}
+
+	override public function setup() {
+		super.setup();
+		// create world
 		final p = Globals.savefile.userProfiles.getProfile('test-${testId}');
 		this.profile = new userdata.Profile(p);
 		final world = new World(Globals.rules, profile);
-		// we will create our world here.
-		super(testId, name, world);
+		this.initWorld(world);
 	}
 }
