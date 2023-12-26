57178802b3ee2f439d000430e5b1ff02b241300b
Eric Ng <ericnjf@gmail.com>
2023-12-26 16:05:48 +0800

    impl/MenuScreen: add start button to menuscreen

--- src/screens/MenuScreen.hx
+++ src/screens/MenuScreen.hx
@@ -3,6 +3,17 @@ package screens;
 class MenuScreen extends zf.Screen {
 	public function new() {
 		super();
+
+		final button = zf.ui.Button.fromColor({
+			defaultColor: K.getColor(".white"),
+			width: 80,
+			height: 20,
+			text: "Start",
+			textColor: K.getColor(".black"),
+			font: A.getFont("display", 1),
+		});
+		this.addChild(button);
+		button.setX(100).setY(50);
 	}
 
 	override public function update(dt: Float) {}
